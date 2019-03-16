aa9430d6f44b71166908250346e46d8eb34ea469
Marco Cordeiro <37083749+cordeiromarco@users.noreply.github.com>
2019-03-16 19:27:36 -0300

    Update lamp-server-bootstrap.sh

--- lamp-server-bootstrap.sh
+++ lamp-server-bootstrap.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -ex
 yum update -y
 yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
+yum install -y git wget
 service httpd start
 chkconfig httpd on
 groupadd www
@@ -9,3 +10,5 @@ chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www/ -type f -exec chmod 0664 {} +
+cd /var/www/html
+git clone https://github.com/cordeiromarco/webresume.git
