4c3f021f7ec01a6f2e8659617629666a83626fa0
Marco Cordeiro <37083749+cordeiromarco@users.noreply.github.com>
2019-05-14 11:05:09 -0300

    Update script to create default index and phpinfo

--- lamp-server-bootstrap.sh
+++ lamp-server-bootstrap.sh
@@ -1,17 +1,25 @@
 #! /bin/bash -ex
+
+#Install LAMP server and update 
 yum update -y
 yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
 yum install -y git wget
+
+#Start HTTPD
 service httpd start
 chkconfig httpd on
+
+#Define WWW group and add users
 groupadd www
 usermod -a -G www ec2-user
 chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www/ -type f -exec chmod 0664 {} +
-cd /var/www/html
-git clone https://github.com/cordeiromarco/webresume.git
 
 #Create a PHP page with the following commands:
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+
+#create a default web page 
+cp /usr/share/httpd/noindex/index.html /var/www/html/index.html
+
