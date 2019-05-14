7bf4da32e5e79b7ff9260d7c1992dbe5a22ea3d9
Marco Cordeiro <37083749+cordeiromarco@users.noreply.github.com>
2019-05-14 11:09:18 -0300

    disable firewalld and selinux

--- lamp-server-bootstrap.sh
+++ lamp-server-bootstrap.sh
@@ -17,6 +17,13 @@ chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www/ -type f -exec chmod 0664 {} +
 
+#Disable SElinux
+sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+
+#Disable Firewalld
+service firewalld stop
+chkconfig firewalld off
+
 #Create a PHP page with the following commands:
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 
