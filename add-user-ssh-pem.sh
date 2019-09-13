d8d9661d8a4222a6ede51b628d83371ab9a93709
Marco Cordeiro <37083749+cordeiromarco@users.noreply.github.com>
2019-09-13 15:38:04 -0300

    NOPASSWD:ALL

--- add-user-ssh-pem.sh
+++ add-user-ssh-pem.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # Run me with superuser privileges
-echo 'admin	ALL=(ALL:ALL) ALL' >> /etc/sudoers
+echo '$1  ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
+#echo 'admin	ALL=(ALL:ALL) ALL' >> /etc/sudoers
 mkdir /etc/skel/.ssh
 chmod 700 /etc/skel/.ssh
 useradd -G admin -m --skel /etc/skel/ $1
